b1c0127eaa89ebba92c656511bad46d28779c3ce
GrapeJelly <james.m.bond1@gmail.com>
2024-07-31 16:43:57 -0400

    Fixed the Repo, had two slns by accident

--- SpinButton.cs
+++ SpinButton.cs
@@ -1,26 +1,37 @@
 using Godot;
 using System;
 
-public partial class SpinButton : Button
+public partial class SpinButton : Button 
 {
 	[Export]
-	Node wheel;
-	
+	// Node2D wheel;
+	Sprite2D wheel;
+
+
+	bool startRotation = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		base._Ready();
 		this.Pressed += PressButton;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		base._Process(delta);
+		if (startRotation)
+		{
+			GD.Print("entered if statement");
+			GD.Print(wheel.RotationDegrees);
+			wheel.RotationDegrees += 1;
+		}
 	}
 
 	private void PressButton()
 	{
-
 		GD.Print("Spinning Wheel!");
-		
+		startRotation = true;
 	}
 }
