d2bce5833138552ac6a74a04366d29fe0f3fc6d4
GrapeJelly <james.m.bond1@gmail.com>
2025-08-20 20:29:27 -0400

    Found issue with voting logistics
    (Test vs. Test2 problem)

--- Scripts/Twitch/ToggleChatInputButton.cs
+++ Scripts/Twitch/ToggleChatInputButton.cs
@@ -1,7 +1,9 @@
 using Godot;
 using Godot.Collections;
-using System.Text.Json.Nodes;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 public partial class ToggleChatInputButton : Button
 {
@@ -101,42 +103,95 @@ public partial class ToggleChatInputButton : Button
     /// <param name="message"></param>
     private void OnMessage(Variant message)
     {
-        string messageText = message.ToString();
+        string socketMessageString = message.ToString();
         // GD.Print($"ToggleChatInputButton.cs: {messageText}");
 
-        JsonNode checkText = ParseJson(messageText, "event/type");
+        JsonNode checkText = ParseJson(socketMessageString, "event/type");
+        JsonNode parsedText = null;
+        JsonNode parsedSender = null;
 
+
+        // Need to move Action checks to GameManager, need to create an event for when toggleChatInput becomes TRUE,
+        // so Action checks can know when  to subscribe to the MessageReceived event from the websocket.
         if (checkText?.ToString() == "Action")
         {
-            JsonNode parsedAction = ParseJson(messageText, "data/arguments/actionName");
+            JsonNode parsedAction = ParseJson(socketMessageString, "data/arguments/actionName");
             GD.Print($"ToggleChatInputButton.cs: Action Called: {parsedAction.ToString()}");
         }
         if (checkText?.ToString() == "ChatMessage")
         {
-            JsonNode parsedText = ParseJson(messageText, "data/text");
-            JsonNode parsedSender = ParseJson(messageText, "data/user/login");
+            parsedText = ParseJson(socketMessageString, "data/text");
+            parsedSender = ParseJson(socketMessageString, "data/user/login");
 
             GD.Print($"ToggleChatInputButton.cs: Message Text: {parsedText.ToString()}");
         }
 
-        // Need to split OnMessage up based off type of event coming in (Action vs. ChatMessage)
-        // Setup event call to trigger logic in Game Manager
+        if (parsedText == null || parsedSender == null) return;
+        CheckingForOptionMatch(parsedText.ToString(), parsedSender.ToString());        
+    }
 
+    private void CheckingForOptionMatch(string chatMessage, string sender)
+    {
+        if (users.Contains(sender))
+        {
+            GD.Print($"ToggleChatInputButton.cs: User {sender} has already voted");
+            return;
+        }
+        users.Add(sender);
 
-        // Don't care about bottom half rn
-
-        string sender = "";
-
+        string[] optionNames = new string[gameManager.CreatedOptions.Count];
         foreach (Option option in gameManager.CreatedOptions)
         {
-            if((messageText.ToLower()).Contains(option.OptionName.ToLower()) && !users.Contains(sender)) 
+            optionNames[gameManager.CreatedOptions.IndexOf(option)] = option.OptionName;
+        }
+
+        int optionResult = OptionPicker(optionNames, chatMessage);
+        if (optionResult != -1)
+        {
+            Option currentOption = gameManager.CreatedOptions[optionResult];
+            users.Add(sender);
+            ++currentOption.OptionWeight;
+            currentOption.UpdateOptionFields();
+        }
+    }
+
+    public int OptionPicker(string[] optionNames, string searchKey)
+    {
+        searchKey = searchKey.ToLower();
+        for (int i = 0; i < optionNames.Length; i++)
+        {
+            optionNames[i] = optionNames[i].ToLower();
+        }
+
+        List<int> candidates = new List<int>();
+
+        for (int i = 0; i < optionNames.Length; i++)
+        {
+            if (searchKey.Contains(optionNames[i]))
             {
-                users.Add(sender);
-                ++option.OptionWeight;
-                option.UpdateOptionFields();
-                return;
+                candidates.Add(i);
             }
         }
+
+        if (candidates.Count == 0)
+        {
+            return -1;
+        }
+
+        int finalCandidate = candidates[0];
+        int finalCandidateLength = optionNames[candidates[0]].Length;
+        for (int i = 1; i < candidates.Count; i++)
+        {
+            int curCandidateLength = optionNames[candidates[i]].Length;
+            if (curCandidateLength > finalCandidateLength)
+            {
+                finalCandidate = candidates[i];
+                finalCandidateLength = curCandidateLength;
+            }
+        }
+
+
+        return finalCandidate;
     }
 
     private JsonNode ParseJson(string messageToParse, string dataPath)
